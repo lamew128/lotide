7c3807473956b9ef1c38dc5837168ffb42435ef9
Eugene <lamew128@gmail.com>
2022-04-06 14:09:22 -0400

    assertObjectsEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,9 +16,9 @@ const assertArraysEqual = function(array1 ,array2) {
   console.log("array2 = " + array2);
 
   if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed: [${array1}] === [${array2}]`);
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
   } else {
-    console.log(`Assertion Failed: [${array1}] !== [${array2}]`);
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
   }
 };
 
