34b8fa81f07080dcdff2aba4107962a94fbbba8f
Eugene <lamew128@gmail.com>
2022-04-08 15:08:44 -0400

    rec

--- eqArrays.js
+++ eqArrays.js
@@ -7,19 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-
   if (array1.length !== array2.length) {
     return false;
   }
   for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); 
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
